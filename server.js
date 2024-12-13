1f97f43c7894aecb5d7a3838acaf6a75e94c46fc
KeiAzDev <enopiyo3code+work@gmail.com>
2024-12-13 12:09:38 -0800

    connected MongoDB

--- server.js
+++ server.js
@@ -4,6 +4,15 @@ const userRoute = require("./routes/users");
 const authRoute = require("./routes/auth");
 const postRoute = require("./routes/posts");
 const PORT = 3000;
+const mongoose = require("mongoose");
+require("dotenv").config();
+
+mongoose.connect(process.env.MONGO_URL).then(() => {
+    console.log("DBと接続中");
+  })
+  .catch((err) => {
+    console.log("err");
+  });
 
 app.use("/api/users", userRoute);
 app.use("/api/auth", authRoute);
