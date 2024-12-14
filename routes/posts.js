9d3c9103b3d8e5d5656e902961c5f9d49c669b90
KeiAzDev <enopiyo3code+work@gmail.com>
2024-12-14 11:20:37 -0800

    delete route added

--- routes/posts.js
+++ routes/posts.js
@@ -27,4 +27,18 @@ router.put("/:id", async (req, res) => {
   }
 });
 
+router.delete("/:id", async (req, res) => {
+  try {
+    const post = await Post.findById(req.params.id);
+      if(post.userId === req.body.userId) {
+        await post.deleteOne();
+        return res.status(200).json("投稿の削除に成功しました");
+      } else {
+        return res.status(403).json("他人の投稿は削除できません");
+      }
+  } catch(err) {
+    return res.status(500).json(err);
+  }
+});
+
 module.exports = router;
